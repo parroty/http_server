37af5660de1fcbbd72491d88e473824c8e5af407
parroty <parroty00@gmail.com>
2013-11-04 18:41:30 +0900

    Loosen elixir version dependency

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HttpServer.Mixfile do
   def project do
     [ app: :http_server,
       version: "0.0.1",
-      elixir: "~> 0.11.0",
+      elixir: ">= 0.10.3",
       deps: deps(Mix.env)
     ]
   end
