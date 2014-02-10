cd1dd9aa9c6d805f344a7d77cd32958e9396911a
Eduardo Gurgel <edgurgel@gmail.com>
2014-02-09 22:04:08 -0300

    Remove unecessary start_deps

--- lib/http_server.ex
+++ lib/http_server.ex
@@ -4,8 +4,6 @@ defmodule HttpServer do
   def start, do: start([], [])
   def start(args), do: start([], args)
   def start(_type, args) do
-    start_deps
-
     path = args[:path] || "/"
     port = args[:port] || 8080
     HttpServer.Handler.define_response(args[:response], args[:wait_time])
@@ -19,13 +17,6 @@ defmodule HttpServer do
         [{:port, port}], [{:env, [{:dispatch, dispatch}]}]
 
     HttpServer.Supervisor.start_link
-  end
-
-  defp start_deps do
-    :application.start(:ranch)
-    :application.start(:crypto)
-    :application.start(:cowlib)
-    :application.start(:cowboy)
   end
 
   def stop, do: stop(nil)
