fbc68ed552996e0d38863e8f9e59dfd64df43d88
parroty <parroty00@gmail.com>
2014-08-07 10:51:28 +0900

    Updates for 0.15.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HttpServer.Mixfile do
   def project do
     [ app: :http_server,
       version: "0.0.1",
-      elixir: "~> 0.14.1",
+      elixir: "~> 0.14.1 or ~> 0.15.0",
       deps: deps(Mix.env)
     ]
   end
